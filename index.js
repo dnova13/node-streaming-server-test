ad54c51b1ce0889a4218084489093ad47f8bd147
dnova13 <dnova13@gmail.com>
2020-12-08 18:20:40 +0900

    111

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
+const fs = require('fs');
+const hls = require('hls-server');
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -41,6 +42,84 @@ app.get("/video", function (req, res) {
 
   // Stream the video chunk to the client
   videoStream.pipe(res);
+});
+
+
+app.get("/video/:name", function (req, res) {
+
+  const {
+    params: { name }
+  } = req;
+
+  // Ensure there is a range given for the video
+  const range = req.headers.range;
+
+  if (!range) {
+    res.status(400).send("Requires Range header");
+  }
+
+  // get video stats (about 61MB)
+  // name : bigbuck.mp4
+  // name : 10m.mp4
+  const videoPath = name;
+  const videoSize = fs.statSync(name).size;
+
+  // Parse Range
+  // Example: "bytes=32324-"
+  const CHUNK_SIZE = 10 ** 6; // 1MB
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+  // console.log(CHUNK_SIZE);
+  // console.log("range : " , range);
+  // console.log("s : " , start);
+  // console.log("e : ", end);
+
+  // Create headers
+  const contentLength = end - start + 1;
+  const headers = {
+    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4",
+  };
+
+  // HTTP Status 206 for Partial Content
+  res.writeHead(206, headers);
+
+  // create video read stream for this particular chunk
+  const videoStream = fs.createReadStream(videoPath, { start, end });
+
+  // Stream the video chunk to the client
+  videoStream.pipe(res);
+});
+
+new hls(server, {
+  provider: {
+    exists: (req, cb) => {
+      const ext = req.url.split('.').pop();
+
+      if (ext !== 'm3u8' && ext !== 'ts') {
+        return cb(null, true);
+      }
+
+      fs.access(__dirname + req.url, fs.constants.F_OK, function (err) {
+        if (err) {
+          console.log('File not exist');
+          return cb(null, false);
+        }
+        cb(null, true);
+      });
+    },
+    getManifestStream: (req, cb) => {
+      const stream = fs.createReadStream(__dirname + req.url);
+      cb(null, stream);
+    },
+    getSegmentStream: (req, cb) => {
+      const stream = fs.createReadStream(__dirname + req.url);
+      cb(null, stream);
+    }
+  }
 });
 
 let port = 3001;
