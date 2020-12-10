dd08e2b3b46a17629269af8978bd716c18d5a15a
dnova13 <dnova13@gmail.com>
2020-12-10 12:10:15 +0900

    streaming test

--- hlstest.js
+++ hlstest.js
@@ -7,6 +7,25 @@ const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 // const ffmpeg = require('@ffmpeg-installer/ffmpeg');
 const hls = require('hls-server');
 
+
+const multer = require("multer");
+const uploader = multer({dest : 'uploads/'});
+// const mediaUp = multer({dest : 'upload/'});
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/var/www/html')
+    },
+    filename: function (req, file, cb) {
+
+        console.log(file);
+      cb(null, Date.now() + ".mp4")
+    }
+});
+
+const mediaServer = multer({storage : storage});
+// const mediaServer = multer({dest : '/var/www/html'});
+
 const moment = require('moment');
 
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
@@ -27,7 +46,7 @@ app.get('/hls/:name', async (req, res) => {
         '-level 3.0',
         // '-s 640x360',          // 640px width, 360px height output video dimensions
         '-start_number 0',     // start the first .ts segment at index 0
-        '-hls_time 30',        // 10 second segment duration
+        '-hls_time 100',        // 10 second segment duration
         '-hls_list_size 0',    // Maxmimum number of playlist entries (0 means all entries/infinite)
         '-f hls'               // HLS format
     ]).on('start', function(commandLine) {
@@ -40,8 +59,38 @@ app.get('/hls/:name', async (req, res) => {
         // fs.rmdirSync('./videos/'+ id);
         // deleteFolderRecursive('./videos/'+ id)
         console.log(err.message);
+    }).on('end', () => {
+        console.log("end");
     }).saveToFile(`./videos/${id}/${id}.m3u8`); //저장
 });
+
+// 업로드
+app.post("/upload/media", mediaServer.single('media'), (req, res, next) => {
+
+    // 3. 파일 객체
+    let file = req.file
+
+    console.log(file);
+    file.downloadPath = `http://192.168.0.61/${file.filename}`
+
+    // 4. 파일 정보
+    let result = {
+        originalName : file.originalname,
+        size : file.size,
+    }
+
+    res.json(file);
+
+    // console.log(req.param("name"));
+    // res.sendFile(`${__dirname}/${req.param("name")}`);
+});
+
+app.post("/upload/medias", uploader.array('medias',20), (req, res, next) => {
+
+    // console.log(req.param("name"));
+    // res.sendFile(`${__dirname}/${req.param("name")}`);
+});
+
 
 
 app.use('/stream',express.static('./videos'));
@@ -54,7 +103,7 @@ app.use('/stream',express.static('./videos'));
 //     res.sendFile(`${__dirname}/videos/${id}/${id}.m3u8`);
 // });
 
-let port = 3002;
+let port = 3001;
 
 app.listen(port, function () {
     console.log(`Listening on port ${port}!`);
